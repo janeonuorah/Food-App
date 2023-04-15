3b598ad9f74a2f987f574331a52c474e012d0bfa
Onyinyechi Onuorah <janeonuorah2000@gmail.com>
2023-04-15 18:30:39 +0100

    added details screen

--- src/externalData/externalData.js
+++ src/externalData/externalData.js
@@ -1,69 +1,81 @@
 const categories = [
-    {
-      id: 0,
-      title: "Grilled Cheese Salad With Cream",
-      image: require("../assets/Images/food4.jpg"),
-      price: 15.50,
-      rating: 4.7,
+  {
+    id: 0,
+    title: "Tasty Beef Burrito with Freshly Pressed Orange Juice",
+    image: require("../assets/Images/Tasty-Beef-Burrito.jpg"),
+    price: 15.50,
+    rating: 4.7,
+    distance: '3.2km',
+    approximateTime: '10-20 Min',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed interdum ornare erat a convallis. Nam pharetra nisl nec risus malesuada, eget ultrices justo vestibulum. Ut sagittis venenatis mi ac mollis. Aliquam porta dolor lectus, a pulvinar mi porta ut. Cras posuere ut mauris sed consectetur. Nunc nec lobortis sapien, eget varius nisi. Sed tincidunt eros a leo pulvinar, nec hendrerit dui commodo.',
     },
-    {
-      id: 1,
-      title: "Seafood Linguine",
-      image: require("../assets/Images/food3.jpg"),
-      price: 25,
-      rating: 3.7
+  {
+    id: 1,
+    title: "Seafood Linguine",
+    image: require("../assets/Images/Seafood-Linguine.jpg"),
+    price: 25,
+    rating: 3.7,
+    distance: '4.2km',
+    approximateTime: '20-30 Min',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed interdum ornare erat a convallis. Nam pharetra nisl nec risus malesuada, eget ultrices justo vestibulum. Ut sagittis venenatis mi ac mollis. Aliquam porta dolor lectus, a pulvinar mi porta ut. Cras posuere ut mauris sed consectetur. Nunc nec lobortis sapien, eget varius nisi. Sed tincidunt eros a leo pulvinar, nec hendrerit dui commodo.',
     },
-    {
-      id: 2,
-      title: "Steamed Vegetables",
-      image: require("../assets/Images/food2.jpg"),
-      price: 15,
-      rating: 4.3
+  {
+    id: 2,
+    title: "Steamed Vegetables and Beef",
+    image: require("../assets/Images/Steamed-Veggies.jpg"),
+    price: 15,
+    rating: 4.3,
+    distance: '1.2km',
+    approximateTime: '5-20 Min',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed interdum ornare erat a convallis. Nam pharetra nisl nec risus malesuada, eget ultrices justo vestibulum. Ut sagittis venenatis mi ac mollis. Aliquam porta dolor lectus, a pulvinar mi porta ut. Cras posuere ut mauris sed consectetur. Nunc nec lobortis sapien, eget varius nisi. Sed tincidunt eros a leo pulvinar, nec hendrerit dui commodo.',
     },
-    {
-      id: 3,
-      title: "Protein Soup",
-      image: require("../assets/Images/food1.jpg"),
-      price: 30,
-      rating: 5.0
+  {
+    id: 3,
+    title: "Protein Soup",
+    image: require("../assets/Images/Protein-Soup.jpg"),
+    price: 30,
+    rating: 5.0,
+    distance: '0.5km',
+    approximateTime: '1-10 Min',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed interdum ornare erat a convallis. Nam pharetra nisl nec risus malesuada, eget ultrices justo vestibulum. Ut sagittis venenatis mi ac mollis. Aliquam porta dolor lectus, a pulvinar mi porta ut. Cras posuere ut mauris sed consectetur. Nunc nec lobortis sapien, eget varius nisi. Sed tincidunt eros a leo pulvinar, nec hendrerit dui commodo.',
     },
-  ];
+];
 
-  
-  const nearFoods = [
-    {
-      id: 0,
-      title: "Jollof Rice",
-      image: require("../assets/Images/jollof-rice.jpeg"),
-      price: 10.50,
-      rating: 4.7,
-      location: 'Dutse • 1km',
-    },
-    {
-      id: 1,
-      title: "Fufu and Afang Soup",
-      image: require("../assets/Images/afang-soup.jpeg"),
-      price: 20,
-      rating: 5.0,
-      location: 'Kubwa • 2km',
-    },
-    {
-      id: 2,
-      title: "Yam and Egg Sauce",
-      image: require("../assets/Images/yam.jpeg"),
-      price: 10.00,
-      rating: 4.0,
-      location: 'Wuse • 0.5km',
-    },
-    {
-      id: 3,
-      title: "Small Chops Platter ",
-      image: require("../assets/Images/small-chops.jpeg"),
-      price: 17.00,
-      rating: 4.1,
-      location: 'Gwarimpa • 1.5km',
-    },
-  ];
 
-  
-  export  {categories, nearFoods} ;
+const nearFoods = [
+  {
+    id: 0,
+    title: "Jollof Rice",
+    image: require("../assets/Images/jollof-rice.jpeg"),
+    price: 10.50,
+    rating: 4.7,
+    location: 'Dutse • 1km',
+  },
+  {
+    id: 1,
+    title: "Fufu and Afang Soup",
+    image: require("../assets/Images/afang-soup.jpeg"),
+    price: 20,
+    rating: 5.0,
+    location: 'Kubwa • 2km',
+  },
+  {
+    id: 2,
+    title: "Yam and Egg Sauce",
+    image: require("../assets/Images/yam.jpeg"),
+    price: 10.00,
+    rating: 4.0,
+    location: 'Wuse • 0.5km',
+  },
+  {
+    id: 3,
+    title: "Small Chops Platter ",
+    image: require("../assets/Images/small-chops.jpeg"),
+    price: 17.00,
+    rating: 4.1,
+    location: 'Gwarimpa • 1.5km',
+  },
+];
+
+
+export { categories, nearFoods };
--- src/screens/Details.js
+++ src/screens/Details.js
@@ -1,22 +1,238 @@
-import { View, Text, StyleSheet } from 'react-native'
-import React from 'react'
+import { View, Text, StyleSheet, ScrollView, Image, Pressable, TouchableOpacity } from 'react-native'
+import React, { useState } from 'react'
 
-const FoodDetails = () => {
+const FoodDetails = ({ route }) => {
+  const item = route.params.foodItemDetails;
+
+  const [showFullDetails, setShowFullDetails] = useState();
+
+  const onReadMorePress = () => {
+    setShowFullDetails(!showFullDetails);
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Details Coming Soon</Text>
+    <View>
+
+      <ScrollView>
+        <View style={styles.container}>
+          {/*Back Button and Cart Bag */}
+          <View style={styles.BackButtonAndCartBagContainer}>
+            <Image source={require('../assets/Icon/back-arrow.png')} style={styles.backButton}></Image>
+            <Image source={require('../assets/Icon/shopping-bag.png')} style={styles.shoppingBag}></Image>
+          </View>
+
+          {/*Food Image/ First Part of Details Page */}
+          <Image source={item.image} style={styles.FoodImage} />
+
+          <View style={{ marginLeft: 10, marginRight: 10 }}>
+
+            <View style={styles.NameAndOrderAmountContainer}>
+
+              {/*Second Part of Details Page */}
+              <View style={{ flexBasis: '70%' }}>
+                <Text style={styles.FoodName}>{item.title}</Text>
+              </View>
+
+              {/*Add and Remove Buttons and Items for shopping */}
+              <View style={styles.addToCartContainer}>
+                <Pressable style={styles.removeButton}><Text style={{ color: '#00b4d8' }}>-</Text></Pressable>
+                <Text style={styles.numberOrdered}>1</Text>
+                <Pressable style={styles.addButton}><Text style={{ color: 'white' }}>+</Text></Pressable>
+              </View>
+            </View>
+
+            {/*Third Part of Details Page containing distance, rating and approx. time */}
+            <View style={{ flexDirection: 'row', justifyContent: 'space-between', marginTop: 20 }}>
+
+              <View style={styles.perDistanceRatingAppoxTimeContainer}>
+                <Image source={require('../assets/Icon/location.png')} style={styles.distanceRatingAppoxTimeIcon} />
+                <Text style={styles.distanceRatingAppoxTimeText}>{item.distance}</Text>
+              </View>
+
+              <View style={styles.perDistanceRatingAppoxTimeContainer}>
+                <Image source={require('../assets/Icon/details-star.png')} style={styles.distanceRatingAppoxTimeIcon} />
+                <Text style={styles.distanceRatingAppoxTimeText}>{item.rating} Ratings</Text>
+              </View>
+
+              <View style={styles.perDistanceRatingAppoxTimeContainer}>
+                <Image source={require('../assets/Icon/food-cover.png')} style={styles.distanceRatingAppoxTimeIcon} />
+                <Text style={styles.distanceRatingAppoxTimeText}>{item.approximateTime}</Text>
+              </View>
+
+            </View>
+
+            {/*Food Item Description */}
+            <Text style={{ marginVertical: 20, fontSize: 20, fontWeight: 'bold' }}>Description</Text>
+
+            <View>
+              {/*Ternary condition to show short food details */}
+              <Text style={{ lineHeight: 25, fontSize: 17, color: 'grey', fontWeight: '400' }}
+                numberOfLines={showFullDetails ? undefined : 3}
+                ellipsizeMode='clip'
+              >
+                {showFullDetails ? item.description : item.description}
+              </Text>
+
+              {/*Conditions to show more or less of food details */}
+              {showFullDetails ? (
+                <TouchableOpacity onPress={onReadMorePress}>
+                  <Text style={{ color: '#00b4d8', fontSize: 15, fontWeight: '600' }}>Show less...</Text>
+                </TouchableOpacity>
+              ) : (
+                <TouchableOpacity onPress={onReadMorePress}>
+                  <Text style={{ color: '#00b4d8', fontSize: 15, fontWeight: '600' }}>Read more...</Text>
+                </TouchableOpacity>
+              )}
+            </View>
+
+            <Image source={require('../assets/Images/map.jpeg')} style={{ width: '100%', height: 300, resizeMode: 'cover', marginVertical: 20 }} />
+
+          </View>
+
+        </View>
+
+      </ScrollView >
+
+      <View style={styles.lowerButtonsContainer}>
+        <TouchableOpacity style={{backgroundColor:'white', borderWidth: 2, borderRadius: 20, borderColor: '#00b4d8'}}><Text style={[styles.buttonsChartandAddCart, styles.chatButton]}>Chat</Text></TouchableOpacity>
+        <TouchableOpacity style={{backgroundColor:'#00b4d8', borderWidth: 2, borderRadius: 20, borderColor: '#00b4d8'}}><Text style={[styles.buttonsChartandAddCart, styles.addCartButton]}>Add Cart</Text></TouchableOpacity>
+      </View>
+
     </View>
+
   )
 }
 
-
 const styles = StyleSheet.create({
- container: {
+  container: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    textAlign: "center",
+    padding: 8
   },
+
+  FoodImage: {
+    width: 400,
+    height: 400,
+    borderRadius: 30,
+  },
+
+  BackButtonAndCartBagContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    top: 70,
+    zIndex: 1,
+    marginLeft: 20,
+    marginRight: 20
+  },
+
+  backButton: {
+    height: 30,
+    width: 30,
+    backgroundColor: 'white',
+    borderRadius: 10,
+    padding: 10
+  },
+
+  shoppingBag: {
+    height: 30,
+    width: 30,
+    backgroundColor: 'white',
+    borderRadius: 8,
+    padding: 5
+  },
+
+  NameAndOrderAmountContainer: {
+    marginTop: 10,
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    flexWrap: 'wrap',
+  },
+
+  FoodName: {
+    fontSize: 23,
+    fontWeight: 'bold',
+  },
+
+  addToCartContainer: {
+    flexDirection: 'row',
+    backgroundColor: '#e2e2e2',
+    justifyContent: 'space-between',
+    width: 80,
+    height: 40,
+    alignItems: 'center',
+    paddingLeft: 10,
+    paddingRight: 10,
+    borderRadius: 20,
+    flexBasis: '30%',
+    marginTop: 10
+  },
+
+  numberOrdered: {
+    fontWeight: 'bold',
+    fontSize: 20
+  },
+
+  addButton: {
+    backgroundColor: '#00b4d8',
+    paddingHorizontal: 5,
+    paddingVertical: 1,
+    borderRadius: 10,
+  },
+
+  removeButton: {
+    backgroundColor: 'white',
+    paddingHorizontal: 5,
+    paddingVertical: 1,
+    borderRadius: 10,
+  },
+
+  perDistanceRatingAppoxTimeContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center'
+  },
+
+  distanceRatingAppoxTimeIcon: {
+    flexDirection: 'row',
+    height: 20,
+    width: 20
+  },
+
+  distanceRatingAppoxTimeText: {
+    fontSize: 15,
+    color: 'grey',
+    fontWeight: '500'
+  },
+
+  lowerButtonsContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    top: -80,
+    paddingBottom: 30,
+    paddingTop: 10,
+    paddingHorizontal: 30,
+    backgroundColor: 'white',
+  },
+
+  buttonsChartandAddCart: {
+    marginLeft: 20,
+    marginBottom: 10,
+    paddingHorizontal: 20,
+    paddingVertical: 10,
+    borderRadius: 20,
+    fontSize: 25,
+    fontWeight: 'bold',
+  },
+
+  chatButton: {
+    color: '#00b4d8',
+    marginRight: 20
+  },
+
+  addCartButton: {
+   color: 'white',
+   marginRight: 20
+  }
 })
 
 export default FoodDetails
